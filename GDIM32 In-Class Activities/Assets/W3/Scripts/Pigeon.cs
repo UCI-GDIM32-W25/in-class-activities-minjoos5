5bf22ee9c1ebb9a64d48742ca4d11b5bff19d3c4
minjoos5 <minjoos5@uci.edu>
2026-01-20 18:15:42 -0800

    Activity 2 done

--- GDIM32 In-Class Activities/Assets/W3/Scripts/Pigeon.cs
+++ GDIM32 In-Class Activities/Assets/W3/Scripts/Pigeon.cs
@@ -9,7 +9,8 @@ public class Pigeon : MonoBehaviour
     [SerializeField] private Animator _animator;
 
     // (1) add a member variable to represent the Pigeon's state
-    
+
+    private MovementState _currentActivity;
 
     void Update()
     {
@@ -22,7 +23,14 @@ public class Pigeon : MonoBehaviour
     // - otherwise, it should be Idle
     private void UpdateState ()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _currentActivity = MovementState.Flying;
+        }
+        else
+        {
+            _currentActivity = MovementState.Idle;
+        }
     }
 
     // (3) fill in this method to update the pigeon's animation based on its state
@@ -31,7 +39,16 @@ public class Pigeon : MonoBehaviour
     // use a Switch statement!
     private void UpdateAppearance()
     {
-        
+        switch (_currentActivity)
+        {
+            case MovementState.Flying:
+            PlayFlyAnimation();
+            break;
+
+            case MovementState.Idle:
+            PlayIdleAnimation();
+            break;
+        }
     }
 
     private void PlayFlyAnimation () {
